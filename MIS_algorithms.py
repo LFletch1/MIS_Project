8d8a4ef172071667014a8ead5263d9ae7a5cb48b
LFletch1 <lgfletcher1@buffs.wtamu.edu>
2022-11-18 18:48:57 -0600

    Nearly done with new MIS algorithm. Its slightly wrong, but its close. Doing roughly 5% better than luby's

--- MIS_algorithms.py
+++ MIS_algorithms.py
@@ -3,7 +3,7 @@ import random as rand
 import matplotlib.pyplot as plt
 
 def lubys_MIS(G):
-    G_ = nx.Graph(G)
+    G_ = nx.Graph(G) # Create copy of graph that can be manipulated
     empty_graph = False
     I = []
     rounds = 1
@@ -37,11 +37,107 @@ def lubys_MIS(G):
             G_.remove_node(y)
      
         if G_.number_of_nodes() == 0:
-            print("Total Rounds:", rounds)
+            # print("Total Rounds:", rounds)
             empty_graph = True
         else:
             rounds += 1
 
     return I, rounds
 
-# def verify_mis():
+def degree_1_MIS(G):
+    G_ = nx.Graph(G) # Create copy of graph that can be manipulated
+    node_ranks =  rand.sample(range(0, G_.number_of_nodes()+1), G_.number_of_nodes())
+    empty_graph = False
+    MIS = []
+    total_one_degree_rounds = 0
+    total_MIS_rounds = 0
+    while not empty_graph:
+        # Add all degree one vertices to the MIS. Then remove all of these nodes and their neighbors from the graph.
+        # Continue doing this until no degree node one vertices remain. Note: Non-one degree nodes can become degree one
+        # during this process. Similar to k-core.
+        
+        one_degree_nodes_exist = True
+        k = 0
+        while one_degree_nodes_exist: # Could potentially set this to a limit of k iterations
+            neighbors_to_remove = []
+            I = []
+            pairs = []
+            for n in G_.nodes():
+                n_degree = G_.degree(n)
+                if n_degree == 0:
+                    I.append(n)
+                elif n_degree == 1:
+                    neighbor = [neigh for neigh in G_.neighbors(n)][0]
+                    # print(neighbor)
+                    if G_.degree(neighbor) == 1: # A pair of one degree nodes
+                        pair = (n,neighbor)
+                        if pair not in pairs: # Don't add redundant pairs
+                            pairs.append((n, neighbor)) 
+                    else: 
+                        neighbors_to_remove.append(neighbor)
+                        I.append(n)
+            for pair in pairs:
+                I.append(pair[0])
+                neighbors_to_remove.append(pair[1])
+
+            MIS += I
+
+            for i in I:
+                G_.remove_node(i)
+            for n in neighbors_to_remove:
+                try: # Will potentially try to remove the same neighbors
+                    G_.remove_node(n)
+                except nx.NetworkXError:
+                    pass
+
+            k += 1
+
+            if len(I) == 0:
+                one_degree_nodes_exist = False
+        
+        total_one_degree_rounds += k
+
+        # Once no degree one nodes are present utilize node labels and perform a single random parallel MIS round
+        I = []
+        nodes_to_remove = []
+        total_MIS_rounds += 1
+        for n in G_.nodes():
+            highest_rank = True
+            n_rank = node_ranks[n]
+            potential_neighbors_to_remove = []
+            for neigh in G_.neighbors(n): # Check if n is highest ranked node in set of neighbors. (Higher rank = Greater integer)
+                if node_ranks[neigh] > n_rank:
+                    highest_rank = False
+                    break
+                else:
+                    potential_neighbors_to_remove.append(neigh)
+
+            if highest_rank:
+                I.append(n)
+                nodes_to_remove += potential_neighbors_to_remove
+        
+        for i in I:
+            G_.remove_node(i)
+        for n in nodes_to_remove:
+            try: # Will potentially try to remove the same neighbors
+                G_.remove_node(n)
+            except nx.NetworkXError:
+                pass 
+        MIS += I
+
+        if G_.number_of_nodes() == 0:
+            empty_graph = True        
+            
+    # print(total_one_degree_rounds, "one degree rounds")
+    # print("Number of nodes in G:", G_.number_of_nodes())
+    # print("MIS Size:", len(MIS))
+    return MIS, total_MIS_rounds
+
+
+def valid_MIS(MIS, G):
+    '''Does not check for maximality, just checks that no adjacency violations are present'''
+    for i in MIS:
+        for neigh in G.neighbors(i):
+            if neigh in MIS:
+                return False
+    return True
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import networkx as nx
-from MIS_algorithms import lubys_MIS
+from MIS_algorithms import lubys_MIS, degree_1_MIS, valid_MIS
 
 
 def get_graph_from_file(filename, delim=","):
@@ -13,15 +13,31 @@ def get_graph_from_file(filename, delim=","):
 
 def main():
     G = get_graph_from_file("data\\facebook_combined.txt", " ")
+
+    # MIS = degree_1_MIS(G)
+    # print(valid_MIS(MIS,G))
+    # Degree One MIS
     total = 0
     total_rounds = 0
     total_MIS_sets = 100
     for i in range(total_MIS_sets): 
-        mis, rounds = lubys_MIS(G)
-        total += len(mis)
+        MIS, rounds = degree_1_MIS(G)
+        total += len(MIS)
         total_rounds += rounds
+    print("Degree One MIS")
+    print("Average MIS Size:", total/total_MIS_sets)
+    print("Average Rounds:", total_rounds/total_MIS_sets)
 
-    
+    # Lubys MIS
+    total = 0
+    total_rounds = 0
+    total_MIS_sets = 100
+    for i in range(total_MIS_sets): 
+        MIS, rounds = lubys_MIS(G)
+        total += len(MIS)
+        total_rounds += rounds
+ 
+    print("Luby MIS")
     print("Average MIS Size:", total/total_MIS_sets)
     print("Average Rounds:", total_rounds/total_MIS_sets)
 
